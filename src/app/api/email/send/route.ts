e9513906ee1a80f8fb877da97f520571b556e6d0
Henry Nguyen <riripishu@gmail.com>
2024-02-28 08:17:16 +1000

    oop consolelog

--- src/app/api/email/send/route.ts
+++ src/app/api/email/send/route.ts
@@ -11,7 +11,6 @@ const resend = new Resend(process.env.NEXT_RESEND_API_KEY);
 export async function POST(request: NextRequest) {
   try {
     const requestData: EmailSendBodyType = await request.json()
-    console.log({requestData})
 
     const responseData = await resend.emails.send({
       from: 'bubble <hello@bubblebook.app>',
